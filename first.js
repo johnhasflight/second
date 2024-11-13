582447c576eaf3658704a70c2928c6899152bccd
johnhasflight <olivercrossleyfordham.30@gmail.com>
2024-11-13 14:26:05 +0000

    first.js

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log(Math.floor(Math.random() * 50));
+console.log(Math.floor(Math.random() * 50)); 
+
+console.log(Math.random() * 50);
+
+console.log(Math.random() * 100);
+
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.floor(Math.random() * 100));
+
+console.log(Math.floor(Math.random() * 5000));
